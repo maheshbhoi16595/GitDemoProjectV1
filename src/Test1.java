848a4c0c402613132984a7b81177d962571b1370
Mahesh <Mahesh@DESKTOP-TLKBBGL>
2022-08-11 18:44:38 +0530

    First commit

---
+++ src/Test1.java
@@ -0,0 +1,18 @@
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String str="mahesh";
+		
+		if (str=="mahesh")
+			System.out.println("name currect");
+		else
+		{
+			System.out.println("wrong name");
+		}
+
+	}
+
+}
