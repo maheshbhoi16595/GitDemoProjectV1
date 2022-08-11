b0ed0767814a7387ee493d72f0b45564d67db618
Mahesh <Mahesh@DESKTOP-TLKBBGL>
2022-08-11 21:36:09 +0530

    this is second commit

---
+++ src/Test2.java
@@ -0,0 +1,13 @@
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println(" this is second test");
+		System.out.println("this is also second test");
+
+	}
+	
+
+}
