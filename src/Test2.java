3c7cc12a633d9584cfe4a9cbff1a372b3c5343f5
Mahesh <Mahesh@DESKTOP-TLKBBGL>
2022-08-11 21:42:54 +0530

    commiting test2

--- src/Test2.java
+++ src/Test2.java
@@ -3,11 +3,8 @@ public class Test2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		System.out.println(" this is second test");
-		System.out.println("this is also second test");
 
+		System.out.println("this is my test2");
 	}
-	
 
 }
