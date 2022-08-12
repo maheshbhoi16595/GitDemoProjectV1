248e64a5edd785e61578676081255425220ff30d
Mahesh <Mahesh@DESKTOP-TLKBBGL>
2022-08-12 19:19:29 +0530

    second update

--- src/Mahesh_New.java
+++ src/Mahesh_New.java
@@ -5,6 +5,7 @@ public class Mahesh_New {
 	public static void main(String[] args) {
 		
 		System.out.println("this is convert eclipse to github account");
+		System.out.println("second modify");
 	}
 	
 		
